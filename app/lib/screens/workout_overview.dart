ff60ffb5e133219f0620d1cabd0ecaa809d318c0
Laurens Dewaele <laurens.dewaele@gmail.com>
2019-11-11 17:12:27 +0100

    trying static list. Do not notice difference. Trying on emulator

--- app/lib/screens/workout_overview.dart
+++ app/lib/screens/workout_overview.dart
@@ -29,26 +29,14 @@ class OverviewScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<WorkoutOverviewCard> workoutWidgets = multipleWorkouts
+        .map((workout) => WorkoutOverviewCard(workout: workout));
     final EdgeInsets padding = MediaQuery.of(context).padding;
     final double viewHeight =
         MediaQuery.of(context).size.height - padding.top - padding.bottom;
     return Screen(
         gradientStartColor: styles.Colors.bgGrayStart,
         gradientStopColor: styles.Colors.bgGrayStop,
-        child: ListView.separated(
-          itemCount: multipleWorkouts.length + 2,
-          itemBuilder: (BuildContext context, int index) {
-            if (index < multipleWorkouts.length) {
-              return WorkoutOverviewCard(workout: multipleWorkouts[index]);
-            } else if (index == multipleWorkouts.length) {
-              return Button(
-                  text: 'Add workout', handleClick: _handleAddWorkout);
-            } else {
-              return SizedBox(height: viewHeight / 2 - styles.Measurements.m);
-            }
-          },
-          separatorBuilder: (BuildContext context, int index) =>
-              const Divider(),
-        ));
+        child: ListView(children: <Widget>[...workoutWidgets]));
   }
 }
