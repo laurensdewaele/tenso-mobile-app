c4702fcce3ca103a3c1ddc2bef2cc087e3355fb7
Laurens Dewaele <laurens.dewaele@gmail.com>
2019-11-12 15:45:32 +0100

    top animations working

--- app/lib/widgets/workout_overview_card.dart
+++ app/lib/widgets/workout_overview_card.dart
@@ -1,8 +1,9 @@
-import 'package:app/widgets/workout_overview_card_collapsed.dart';
-import 'package:app/widgets/workout_overview_card_expanded.dart';
 import 'package:flutter/cupertino.dart';
 
 import 'package:app/data/interfaces.dart';
+import 'package:app/styles/styles.dart' as styles;
+import 'package:app/widgets/card.dart';
+import 'package:app/widgets/difficulty.dart';
 
 class WorkoutOverviewCard extends StatefulWidget {
   WorkoutOverviewCard({Key key, this.workout}) : super(key: key);
@@ -19,6 +20,15 @@ class _WorkoutOverviewCardState extends State<WorkoutOverviewCard>
   AnimationController controller;
 
   bool expanded;
+
+  static final Tween<double> _titleAlignmentTween = Tween(begin: -1, end: 0);
+  static final Tween<double> _sizedBoxTween =
+      Tween(begin: styles.Measurements.xxl + styles.Measurements.m);
+
+  // Two moves the difficultyWidget off screen to the right.
+  // It is being clipped, therefore invisible.
+  static final Tween<double> _difficultyAlignmentTween =
+      Tween(begin: 1, end: 2);
 
   @override
   void initState() {
@@ -39,17 +49,17 @@ class _WorkoutOverviewCardState extends State<WorkoutOverviewCard>
   @override
   bool get wantKeepAlive => true;
 
-  void _handleCollapse() {
-    controller.reverse();
-    setState(() {
-      expanded = false;
-    });
-  }
+  void _handleTap() {
+    if (expanded == true) {
+      controller.reverse();
+    }
 
-  void _handleExpand() {
-    controller.forward();
+    if (expanded == false) {
+      controller.forward();
+    }
+
     setState(() {
-      expanded = true;
+      expanded = !expanded;
     });
   }
 
@@ -60,14 +70,74 @@ class _WorkoutOverviewCardState extends State<WorkoutOverviewCard>
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    return expanded
-        ? WorkoutOverviewCardExpanded(
-            workout: widget.workout,
-            handleCollapse: _handleCollapse,
-            handleStart: _handleStart)
-        : WorkoutOverviewCardCollapsed(
-            workout: widget.workout,
-            handleExpand: _handleExpand,
-            animation: animation);
+    return GestureDetector(
+        onTap: _handleTap,
+        child: Card(
+            padding: EdgeInsets.all(styles.Measurements.s),
+            child: Column(children: <Widget>[
+              Container(
+                  child: Stack(overflow: Overflow.clip, children: <Widget>[
+                Row(children: [
+                  Expanded(
+                      child: AnimatedTitle(
+                          animation: animation, tween: _titleAlignmentTween)),
+                  SizedBox(
+                    width: 0,
+                    height: styles.Measurements.xxl,
+                  )
+                ]),
+                AnimatedDifficulty(
+                    animation: animation,
+                    tween: _difficultyAlignmentTween,
+                    difficulty: widget.workout.difficulty.toString(),
+                    difficultyColor: widget.workout.difficultyColor),
+              ]))
+            ])));
+  }
+}
+
+class AnimatedTitle extends AnimatedWidget {
+  AnimatedTitle({Key key, this.animation, this.tween})
+      : super(key: key, listenable: animation);
+
+  final Animation<double> animation;
+  final Tween tween;
+
+  @override
+  Widget build(BuildContext context) {
+    final animation = listenable as Animation<double>;
+    return Align(
+        alignment: Alignment(tween.evaluate(animation), 0),
+        child: Text('small',
+            style: styles.Typography.title, overflow: TextOverflow.ellipsis));
+  }
+}
+
+class AnimatedDifficulty extends AnimatedWidget {
+  AnimatedDifficulty(
+      {Key key,
+      this.animation,
+      this.tween,
+      this.difficulty,
+      this.difficultyColor})
+      : super(key: key, listenable: animation);
+
+  final Animation<double> animation;
+  final Tween tween;
+
+  final String difficulty;
+  final Color difficultyColor;
+
+  @override
+  Widget build(BuildContext context) {
+    final animation = listenable as Animation<double>;
+    return Align(
+        alignment: Alignment(tween.evaluate(animation), 0),
+        child: Difficulty(
+          difficulty: difficulty,
+          difficultyColor: difficultyColor,
+          width: styles.Measurements.xxl,
+          height: styles.Measurements.xxl,
+        ));
   }
 }
--- app/lib/widgets/workout_overview_card_collapsed.dart
+++ app/lib/widgets/workout_overview_card_collapsed.dart
@@ -16,51 +16,5 @@ class WorkoutOverviewCardCollapsed extends StatelessWidget {
   final Animation<double> animation;
 
   @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-        onTap: handleExpand,
-        child: Card(
-            padding: EdgeInsets.all(styles.Measurements.s),
-            child: Stack(overflow: Overflow.clip, children: <Widget>[
-              Row(children: [
-                Expanded(child: AnimatedTitle(animation: animation)),
-                SizedBox(
-                  width: 0,
-                  height: styles.Measurements.xxl,
-                )
-              ]),
-              Align(
-                  alignment: Alignment(1, 0),
-                  child: Difficulty(
-                    difficulty: workout.difficulty.toString(),
-                    difficultyColor: workout.difficultyColor,
-                    width: styles.Measurements.xxl,
-                    height: styles.Measurements.xxl,
-                  )),
-            ])));
-  }
-}
-
-class AnimatedTitle extends AnimatedWidget {
-  AnimatedTitle({Key key, this.animation})
-      : super(key: key, listenable: animation);
-
-  final Animation<double> animation;
-  static final Tween<double> _titleAlignmentTween = Tween(begin: -1, end: 0);
-  static final Tween<double> _sizedBoxTween =
-      Tween(begin: styles.Measurements.xxl + styles.Measurements.m);
-
-  // Two moves the difficultyWidget off screen to the right.
-  // It is being clipped, therefore invisible.
-  static final Tween<double> _difficultyAlignmentTween =
-      Tween(begin: 1, end: 2);
-
-  @override
-  Widget build(BuildContext context) {
-    final animation = listenable as Animation<double>;
-    return Align(
-        alignment: Alignment(_titleAlignmentTween.evaluate(animation), 0),
-        child: Text('small',
-            style: styles.Typography.title, overflow: TextOverflow.ellipsis));
-  }
+  Widget build(BuildContext context) {}
 }
