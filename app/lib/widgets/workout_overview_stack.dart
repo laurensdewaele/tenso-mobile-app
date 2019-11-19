f57ec57c60db96b8c4c159fd5d66db8cc7b3c0e7
Laurens Dewaele <laurens.dewaele@gmail.com>
2019-11-19 13:27:02 +0100

    removed broken listener and removed unecessary setState

--- app/lib/widgets/workout_overview_stack.dart
+++ app/lib/widgets/workout_overview_stack.dart
@@ -7,6 +7,8 @@ import 'package:app/widgets/workout_overview_card.dart';
 import 'package:app/widgets/workout_overview_delete.dart';
 import 'package:app/widgets/workout_overview_edit.dart';
 
+enum _SlideDirection { left, right }
+
 class WorkoutOverviewStack extends StatefulWidget {
   WorkoutOverviewStack({Key key, this.workout}) : super(key: key);
 
@@ -18,25 +20,23 @@ class WorkoutOverviewStack extends StatefulWidget {
 
 class _WorkoutOverviewStackState extends State<WorkoutOverviewStack>
     with SingleTickerProviderStateMixin {
-  static final Animatable<double> _easeInOutTween =
-      CurveTween(curve: Curves.easeInOut);
-  static final Animatable<Offset> _horizontalOffsetTween =
-      Tween(begin: Offset(0.0, 0.0), end: Offset(0.25, 0.0));
+  static final Animatable<double> _easeInTween =
+      CurveTween(curve: Curves.easeIn);
   AnimationController _controller;
-  Animation<Offset> _horizontalOffset;
+  Animation<Offset> _animation;
 
   bool _isSliderOpen = false;
+  double dx = 0.0;
 
   @override
   void initState() {
     super.initState();
 
-    _controller =
-        AnimationController(duration: Duration(milliseconds: 200), vsync: this)
-          ..addStatusListener(_animationStatusListener);
-    _horizontalOffset =
-        _controller.drive(_horizontalOffsetTween.chain(_easeInOutTween));
-    _controller.forward();
+    _controller = AnimationController(vsync: this);
+    _animation = Tween<Offset>(begin: Offset(-.25, 0), end: Offset(.25, 0.0))
+        .animate(_controller);
+    // Neutral position as our controller ranges from [0,1].
+    _controller.value = 0.5;
   }
 
   @override
@@ -45,38 +45,64 @@ class _WorkoutOverviewStackState extends State<WorkoutOverviewStack>
     super.dispose();
   }
 
-  void _onTap() {
-    setState(() {
-      _controller.forward();
-    });
-  }
-
   void _closeSlider() {
     _controller.reverse();
   }
 
-  AnimationStatusListener _animationStatusListener(AnimationStatus status) {
-    switch (status) {
-      case AnimationStatus.dismissed:
-        setState(() {
-          _isSliderOpen = false;
-        });
-        break;
-      case AnimationStatus.completed:
-        setState(() {
-          _isSliderOpen = true;
-        });
-        break;
-      case AnimationStatus.forward:
-        break;
-      case AnimationStatus.reverse:
-        break;
+  void _handleEditTap() {
+    print('triggered');
+    // TODO: Implement modal
+    _close();
+  }
+
+  void _handleDeleteTap() {
+    print('triggered');
+    // TODO: Implement modal
+    _close();
+  }
+
+  void _handleDragStart(DragStartDetails details) {
+    if (_controller.isAnimating) {
+      _controller.stop();
     }
   }
 
-  void _handleEditTap() {}
+  void _handleDragUpdate(DragUpdateDetails details) {
+    dx += details.delta.dx;
+    // An action is a fourth the size of the card
+    final double actionContainerSize = context.size.width / 4;
+    final double traversedPercentage = dx / actionContainerSize;
+    // Our controller range spans from [0,1] and the neutral position is 0.5.
+    final double newControllerValue = .5 + traversedPercentage / 2;
+    _controller.value = newControllerValue;
+  }
 
-  void _handleDeleteTap() {}
+  void _handleDragEnd(DragEndDetails details) {
+    final double velocity = details.primaryVelocity;
+    print(velocity);
+//    final bool shouldOpen = velocity.sign == _dragExtent.sign;
+//    final bool fast = velocity.abs() > widget.fastThreshold;
+  }
+
+  void _animateTo(double value) {
+    _controller.animateTo(value,
+        duration: Duration(milliseconds: 200), curve: Curves.easeIn);
+  }
+
+  void _close() {
+    _animateTo(.5);
+    dx = 0;
+  }
+
+  void _open(_SlideDirection direction) {
+    if (direction == _SlideDirection.left) {
+      _animateTo(0);
+    }
+
+    if (direction == _SlideDirection.right) {
+      _animateTo(1);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -85,9 +111,12 @@ class _WorkoutOverviewStackState extends State<WorkoutOverviewStack>
         _Actions(
             handleDeleteTap: _handleDeleteTap, handleEditTap: _handleEditTap),
         GestureDetector(
-            onTap: () {},
+            onHorizontalDragStart: _handleDragStart,
+            onHorizontalDragUpdate: _handleDragUpdate,
+            onHorizontalDragEnd: _handleDragEnd,
+            behavior: HitTestBehavior.translucent,
             child: SlideTransition(
-                position: _horizontalOffset,
+                position: _animation,
                 child: WorkoutOverviewCard(
                     closeSlider: _closeSlider,
                     workout: widget.workout,
