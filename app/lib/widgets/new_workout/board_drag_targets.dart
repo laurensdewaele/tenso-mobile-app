7d977cddb91a32711cd56608cc09d87861e5aa65
Laurens Dewaele <laurens.dewaele@gmail.com>
2019-12-19 11:32:12 +0100

    removed todos

--- app/lib/widgets/new_workout/board_drag_targets.dart
+++ app/lib/widgets/new_workout/board_drag_targets.dart
@@ -31,13 +31,11 @@ class _BoardDragTargetsState extends State<BoardDragTargets> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
   }
 
   @override
   void dispose() {
-    // TODO: implement dispose
     super.dispose();
   }
 
