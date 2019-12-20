c7228cf589b38107a7720c5fab95a259caa9c7cb
Laurens Dewaele <laurens.dewaele@gmail.com>
2019-12-20 13:15:31 +0100

    realised I need to make the keyboardScreen reusable

---
+++ app/lib/screens/settings.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+
+class SettingsScreen extends StatefulWidget {
+  @override
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
