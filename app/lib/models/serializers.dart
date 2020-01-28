268e36a20b4d06a09c4eaab6328070b44a1ce469
Laurens Dewaele <laurens.dewaele@gmail.com>
2020-01-28 19:43:41 +0100

    yeah buddy boyy

--- app/lib/models/serializers.dart
+++ app/lib/models/serializers.dart
@@ -1,15 +1,18 @@
-import 'package:app/models/fingers.dart';
-import 'package:app/models/grip.dart';
-import 'package:app/models/hand_hold.dart';
-import 'package:app/models/hand_type.dart';
-import 'package:app/models/hold.dart';
-import 'package:app/models/menu_item.dart';
+import 'package:built_collection/built_collection.dart';
 import 'package:built_value/serializer.dart';
 import 'package:built_value/standard_json_plugin.dart';
 
 import 'package:app/models/board.dart';
 import 'package:app/models/board_hold.dart';
+import 'package:app/models/fingers.dart';
+import 'package:app/models/grip.dart';
+import 'package:app/models/hand_hold.dart';
+import 'package:app/models/hand_type.dart';
+import 'package:app/models/hold.dart';
 import 'package:app/models/hold_type.dart';
+import 'package:app/models/menu_item.dart';
+import 'package:app/models/unit.dart';
+import 'package:app/models/workout.dart';
 
 part 'serializers.g.dart';
 
@@ -23,9 +26,8 @@ part 'serializers.g.dart';
   HandType,
   Hold,
   MenuItem,
-//  Sound,
-//  Unit,
-//  Workout
+  Unit,
+  Workout
 ])
 final Serializers serializers =
     (_$serializers.toBuilder()..addPlugin(StandardJsonPlugin())).build();
