1570b003fe7b3b666f72bcb0b71b8a62716d964a
Laurens Dewaele <laurens.dewaele@gmail.com>
2020-02-10 10:04:41 +0200

    you are allowed to crimp on jugs

--- app/lib/functions/board_hold_grip_compatibility.dart
+++ app/lib/functions/board_hold_grip_compatibility.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 
 import 'package:app/models/board_hold.dart';
 import 'package:app/models/grip.dart';
-import 'package:app/models/hold_type.dart';
 
 Widget checkCompatibility(Grip grip, BoardHold boardHold) {
   if (boardHold != null && grip.fingers.count > boardHold.maxAllowedFingers) {
@@ -12,13 +11,6 @@ Widget checkCompatibility(Grip grip, BoardHold boardHold) {
       textAlign: TextAlign.center,
     );
   }
-  if (boardHold != null &&
-      grip.crimped == true &&
-      boardHold.holdType == HoldType.jug) {
-    return Text(
-      'You should not crimp on jugs',
-      textAlign: TextAlign.center,
-    );
-  }
+
   return null;
 }
