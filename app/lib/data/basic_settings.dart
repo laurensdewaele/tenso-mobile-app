c4cd47834012523f797192be6122c35b8fbaa1f0
Laurens Dewaele <laurens.dewaele@gmail.com>
2020-02-25 12:35:13 +0100

    podfile and new settings

--- app/lib/data/basic_settings.dart
+++ app/lib/data/basic_settings.dart
@@ -1,8 +1,14 @@
 import 'package:app/data/boards/beastmaker_1000.dart';
+import 'package:app/data/sounds.dart';
 import 'package:app/models/settings.dart';
 import 'package:app/models/unit.dart';
 
 final Settings basicSettings = Settings((b) => b
   ..defaultBoard = beastmaker1000.toBuilder()
   ..preparationTimer = 20
-  ..unit = Unit.metric);
+  ..unit = Unit.metric)
+  ..hangSound = Sounds.thudDeep
+  ..restSound = Sounds.gong
+  ..beepSound = Sounds.hitLightSoft
+  ..beepsBeforeHang = 5
+  ..beepsBeforeRest = 3;
