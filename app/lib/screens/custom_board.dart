c7681020357d5d5b46259a5bf819f9648cb69c9e
Laurens Dewaele <laurens.dewaele@gmail.com>
2020-04-29 20:52:36 +0200

    board

--- app/lib/screens/custom_board.dart
+++ app/lib/screens/custom_board.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+
+import 'package:app/styles/styles.dart' as styles;
+import 'package:app/widgets/bottom_menu_drawer.dart';
+import 'package:app/widgets/screen.dart';
+import 'package:app/widgets/top_navigation.dart';
 
 class CustomBoardScreen extends StatefulWidget {
   CustomBoardScreen({Key key}) : super(key: key);
@@ -10,18 +16,54 @@ class CustomBoardScreen extends StatefulWidget {
 class _CustomBoardScreenState extends State<CustomBoardScreen> {
   @override
   void initState() {
-    // TODO: implement initState
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+    ]);
     super.initState();
   }
 
   @override
   void dispose() {
-    // TODO: implement dispose
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: <Widget>[
+        Screen(
+          gradientStartColor: styles.Colors.bgGrayStart,
+          gradientStopColor: styles.Colors.bgGrayStop,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              TopNavigation(
+                title: 'custom board',
+                dark: true,
+                handleBackNavigation: () => Navigator.of(context).pop(),
+              ),
+            ],
+          ),
+        ),
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(
+                styles.Measurements.xs, 64, styles.Measurements.xs, 45),
+            child: ClipRRect(
+                borderRadius: styles.kBorderRadiusAll,
+                child: Image.asset('assets/images/boards/custom_board.png')),
+          ),
+        ),
+        BottomMenuDrawer()
+      ],
+    );
   }
 }
