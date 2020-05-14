edb36bb12eae15cdebb8af06a7fe4888c3ebb5e2
Laurens Dewaele <laurens.dewaele@gmail.com>
2020-05-14 07:30:25 +0200

    generic app modal, that's non interactive

--- app/lib/widgets/custom_board/add_hold_modal.dart
+++ app/lib/widgets/custom_board/add_hold_modal.dart
@@ -2,7 +2,7 @@ import 'package:app/styles/styles.dart' as styles;
 import 'package:app/widgets/modal_popup.dart';
 import 'package:flutter/cupertino.dart';
 
-class AddHoldModal extends StatefulWidget {
+class AddHoldModal extends StatelessWidget {
   AddHoldModal({
     Key key,
     @required this.open,
@@ -13,61 +13,16 @@ class AddHoldModal extends StatefulWidget {
   final VoidCallback handleTap;
 
   @override
-  _AddHoldModalState createState() => _AddHoldModalState();
-}
-
-class _AddHoldModalState extends State<AddHoldModal>
-    with SingleTickerProviderStateMixin {
-  AnimationController _animationController;
-  final Animatable<Offset> _offSetTween =
-      Tween<Offset>(begin: Offset(0, 1), end: Offset(0.0, 0.0))
-          .chain(CurveTween(curve: Curves.easeIn));
-
-  @override
-  void initState() {
-    _animationController =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 200));
-    super.initState();
-  }
-
-  @override
-  void didUpdateWidget(AddHoldModal oldWidget) {
-    if (oldWidget.open != widget.open) {
-      if (widget.open == true) {
-        _animationController.forward();
-      } else {
-        _animationController.reverse();
-      }
-    }
-    super.didUpdateWidget(oldWidget);
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return SlideTransition(
-      position: _animationController.drive(_offSetTween),
-      child: GestureDetector(
-        onTap: widget.handleTap,
-        child: AppModal(
-          content: Padding(
-            padding: const EdgeInsets.symmetric(
-                horizontal: styles.Measurements.xs,
-                vertical: styles.Measurements.xs),
-            child: Container(
-              width: double.infinity,
-              child: Text(
-                'add hold',
-                style: styles.Staatliches.xlBlack,
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ),
+    return AppModal(
+      open: open,
+      handleTap: handleTap,
+      content: Container(
+        width: double.infinity,
+        child: Text(
+          'add hold',
+          style: styles.Staatliches.xlBlack,
+          textAlign: TextAlign.center,
         ),
       ),
     );
--- app/lib/widgets/modal_popup.dart
+++ app/lib/widgets/modal_popup.dart
@@ -7,15 +7,15 @@ Future<void> showAppModalPopup({
 }) {
   return showCupertinoModalPopup(
       context: context,
-      builder: (BuildContext context) => AppModal(
+      builder: (BuildContext context) => _AppModalDecoration(
             content: content,
           ));
 }
 
 // Padding can not be generic. We can have buttons inside our modal.
 // Buttons ideally need to be as big as possible.
-class AppModal extends StatelessWidget {
-  AppModal({Key key, @required this.content}) : super(key: key);
+class _AppModalDecoration extends StatelessWidget {
+  _AppModalDecoration({Key key, @required this.content}) : super(key: key);
 
   final Widget content;
 
@@ -27,5 +27,72 @@ class AppModal extends StatelessWidget {
             borderRadius: BorderRadius.only(
                 topLeft: styles.kBorderRadius, topRight: styles.kBorderRadius)),
         child: content);
+  }
+}
+
+class AppModal extends StatefulWidget {
+  AppModal({
+    Key key,
+    @required this.open,
+    @required this.handleTap,
+    @required this.content,
+  }) : super(key: key);
+
+  final bool open;
+  final VoidCallback handleTap;
+  final Widget content;
+
+  @override
+  _AddHoldModalState createState() => _AddHoldModalState();
+}
+
+class _AddHoldModalState extends State<AppModal>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+  final Animatable<Offset> _offSetTween =
+      Tween<Offset>(begin: Offset(0, 1), end: Offset(0.0, 0.0))
+          .chain(CurveTween(curve: Curves.easeIn));
+
+  @override
+  void initState() {
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 200));
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(AppModal oldWidget) {
+    if (oldWidget.open != widget.open) {
+      if (widget.open == true) {
+        _animationController.forward();
+      } else {
+        _animationController.reverse();
+      }
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideTransition(
+      position: _animationController.drive(_offSetTween),
+      child: GestureDetector(
+        onTap: widget.handleTap,
+        child: _AppModalDecoration(
+          content: Padding(
+            padding: const EdgeInsets.symmetric(
+                horizontal: styles.Measurements.xs,
+                vertical: styles.Measurements.xs),
+            child: widget.content,
+          ),
+        ),
+      ),
+    );
   }
 }
