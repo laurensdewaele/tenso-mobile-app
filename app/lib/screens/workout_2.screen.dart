e52c2d4167873baab777bea56045d8dce261477f
Laurens Dewaele <laurens.dewaele@gmail.com>
2020-06-08 13:41:28 +0200

    fast commit message

--- app/lib/screens/workout_2.screen.dart
+++ app/lib/screens/workout_2.screen.dart
@@ -14,6 +14,7 @@ import 'package:app/widgets/section_with_info_icon.dart';
 import 'package:app/widgets/tabs.dart';
 import 'package:app/widgets/text_input.dart';
 import 'package:app/widgets/top_navigation.dart';
+import 'package:app/widgets/workout/fixed_variable_info.dart';
 import 'package:app/widgets/workout/label_picker.dart';
 import 'package:flutter/cupertino.dart' hide Icon;
 
@@ -114,7 +115,7 @@ class _WorkoutScreenState extends State<WorkoutScreen2> {
                           SectionWithInfoIcon(
                             nextSectionHasInfoIcon: true,
                             title: 'rest between groups',
-                            appDialogContent: _RestBetweenGroupsInfo(),
+                            appDialogContent: FixedVariableInfo(),
                             children: <Widget>[
                               Tabs(
                                 leftText: 'Fixed',
@@ -214,57 +215,6 @@ class _ColorLabelInfo extends StatelessWidget {
               handleTap: () {
                 Navigator.of(context).pop();
               }),
-        )
-      ],
-    );
-  }
-}
-
-class _RestBetweenGroupsInfo extends StatelessWidget {
-  _RestBetweenGroupsInfo({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      shrinkWrap: true,
-      physics: ClampingScrollPhysics(),
-      children: <Widget>[
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            RichText(
-              text: TextSpan(text: '', style: styles.Lato.xsBlack, children: [
-                TextSpan(text: 'Fixed ', style: styles.Lato.xsBlackBold),
-                TextSpan(
-                    text:
-                        'will display a countdown timer with a predetermined amount of seconds. This way, you\'re enforcing sufficient and consistent recovery time.',
-                    style: styles.Lato.xsBlack),
-              ]),
-            ),
-            Divider(
-              height: styles.Measurements.m,
-            ),
-            RichText(
-              text: TextSpan(text: '', style: styles.Lato.xsBlack, children: [
-                TextSpan(text: 'Variable ', style: styles.Lato.xsBlackBold),
-                TextSpan(
-                    text:
-                        'will display a stopwatch, so you can decide on the fly how much rest you need.',
-                    style: styles.Lato.xsBlack),
-              ]),
-            ),
-            Divider(
-              height: styles.Measurements.l,
-            ),
-            Button(
-                small: true,
-                displayBackground: false,
-                text: 'Ok',
-                handleTap: () {
-                  Navigator.of(context).pop();
-                })
-          ],
         )
       ],
     );
