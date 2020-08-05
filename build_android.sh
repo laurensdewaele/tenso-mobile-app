06fc4df0e8fb22d3463fab41eeaff8806d5d417d
Laurens Dewaele <laurens.dewaele@gmail.com>
2020-08-05 08:55:56 +0200

    version no bump script

--- build_android.sh
+++ build_android.sh
@@ -1,3 +1,5 @@
+echo Bumping version no.
+perl -i -pe 's/^(version:\s+\d+\.\d+\.)(\d+)(\+)(\d+)$/$1.($2+1).$3.($4+1)/e' pubspec.yaml
 echo Building for android...
 flutter build appbundle
 echo Running fastlane
