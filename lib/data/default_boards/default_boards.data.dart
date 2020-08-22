9889c1af503f0bef5f22ca33794420fb4bb164a4
Laurens Dewaele <laurens.dewaele@gmail.com>
2020-08-22 08:37:20 +0200

    home boy

--- lib/data/default_boards/default_boards.data.dart
+++ lib/data/default_boards/default_boards.data.dart
@@ -1,5 +1,6 @@
 import 'package:tenso_app/data/default_boards/awesome_woodys_cliff_board_mini_back.dart';
 import 'package:tenso_app/data/default_boards/awesome_woodys_cliff_board_mini_front.dart';
+import 'package:tenso_app/data/default_boards/awesome_woodys_the_home_boy.dart';
 import 'package:tenso_app/data/default_boards/beastmaker_1000.data.dart';
 import 'package:tenso_app/data/default_boards/beastmaker_2000.data.dart';
 import 'package:tenso_app/data/default_boards/grindstone_mk2.data.dart';
@@ -11,6 +12,7 @@ final Boards defaultBoards = Boards((b) => b
     beastmaker1000,
     beastmaker2000,
     grindstoneMk2,
+    awesomeWoodysTheHomeBoy,
     awesomeWoodysCliffBoardMiniFront,
     awesomeWoodysCliffBoardMiniBack,
     transgression,
